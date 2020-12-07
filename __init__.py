6f5ac1c571df166ac0f3206c2d80249866cc2ba7
inco <apat2000@gmail.com>
2020-12-06 21:36:24 -0500

    x

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+Support for pfsense gateways status monitoring using PfsenseFauxapi 
+
+Copyright (c) 2020 
+
+Licensed under MIT. All rights reserved.
+
+https://github.com/emberbox/hassio-pfsense-status
+"""
